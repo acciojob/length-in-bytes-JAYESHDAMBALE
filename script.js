8ecfaddfe512f5ec22dba1e2e63a2babb7337b49
Jayesh Dambale <88830136+JAYESHDAMBALE@users.noreply.github.com>
2025-12-31 20:34:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 const byteSize = (str) => {
   // write your code here
+	const blob = new Blob([str]);
+	const inputStringSize = blob.size; 
+
+	return blob.size;
+	
 };
 
 // Do not change the code below
 const str = prompt("Enter some string.");
-alert(byteSize(str));
+alert(byteSize(str)); 
